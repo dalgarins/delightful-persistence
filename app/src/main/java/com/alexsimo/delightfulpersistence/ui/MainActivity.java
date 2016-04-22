731e5baa3a76dfa806896e54af625cb85b842e54
Alexandru Simonescu <alexandru.simonescu@gmail.com>
2016-04-23 00:56:35 +0200

    Moved activities and fragments to own package

--- app/src/main/java/com/alexsimo/delightfulpersistence/ui/MainActivity.java
+++ app/src/main/java/com/alexsimo/delightfulpersistence/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.alexsimo.delightfulpersistence;
+package com.alexsimo.delightfulpersistence.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.alexsimo.delightfulpersistence.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/alexsimo/delightfulpersistence/ui/MainActivityFragment.java
+++ app/src/main/java/com/alexsimo/delightfulpersistence/ui/MainActivityFragment.java
@@ -1,10 +1,11 @@
-package com.alexsimo.delightfulpersistence;
+package com.alexsimo.delightfulpersistence.ui;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.alexsimo.delightfulpersistence.R;
 
 /**
  * A placeholder fragment containing a simple view.
