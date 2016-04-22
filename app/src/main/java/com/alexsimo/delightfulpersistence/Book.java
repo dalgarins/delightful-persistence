1f8c3cff8e16e2b4ea3f3c4ab685b5547900c72d
Alexandru Simonescu <alexandru.simonescu@gmail.com>
2016-04-22 18:29:52 +0200

    Added dependencies: AutoValue, APT

--- app/src/main/java/com/alexsimo/delightfulpersistence/Book.java
+++ app/src/main/java/com/alexsimo/delightfulpersistence/Book.java
@@ -1,7 +1,4 @@
 package com.alexsimo.delightfulpersistence;
 
-/**
- * Created by alexandru.simonescu on 22/4/16.
- */
-public abstract class Book {
+public abstract class Book implements BookModel {
 }
