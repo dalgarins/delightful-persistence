780b52c73f6139331b4f0e374ade9941c019cb0e
Alexandru Simonescu <alexandru.simonescu@gmail.com>
2016-04-23 00:29:25 +0200

    Initialized Stetho on debug

--- app/src/debug/java/com/alexsimo/delightfulpersistence/DebugDelightfulApplication.java
+++ app/src/debug/java/com/alexsimo/delightfulpersistence/DebugDelightfulApplication.java
@@ -1,4 +1,19 @@
 package com.alexsimo.delightfulpersistence;
 
+import com.facebook.stetho.Stetho;
+
 public class DebugDelightfulApplication extends DelightfulApplication {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    initStetho();
+  }
+
+  private void initStetho() {
+    Stetho.initialize(Stetho.newInitializerBuilder(getApplicationContext())
+        .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(getApplicationContext()))
+        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+        .build());
+  }
 }
