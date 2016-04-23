d5b35f15c5201e716fc42dd8b0af129114773420
Alexandru Simonescu <alexandru.simonescu@gmail.com>
2016-04-23 11:45:52 +0200

    Added test cases to AuthorShould to test if table columns have been created

--- app/src/androidTest/java/com/alexsimo/delightfulpersistence/database/AuthorTableShould.java
+++ app/src/androidTest/java/com/alexsimo/delightfulpersistence/database/AuthorTableShould.java
@@ -1,10 +1,14 @@
 package com.alexsimo.delightfulpersistence.database;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import com.alexsimo.delightfulpersistence.database.model.AuthorModel;
 import com.alexsimo.delightfulpersistence.runner.CustomRunner;
 import java.util.HashSet;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class AuthorTableShould extends CustomRunner {
 
@@ -16,8 +20,24 @@ public class AuthorTableShould extends CustomRunner {
 
   @Test
   public void have_created_all_columns() throws Exception {
+    HashSet<String> columns = givenAuthorColumns();
+    SQLiteDatabase db = givenWritableDatabase();
+    Cursor cursor = db.rawQuery("PRAGMA table_info(" + AuthorModel.TABLE_NAME + ")", null);
 
+    cursor.moveToFirst();
+    int columnNameIndex = cursor.getColumnIndex("name");
+    do {
+      String columnName = cursor.getString(columnNameIndex);
+      columns.remove(columnName);
+    } while (cursor.moveToNext());
 
+    assertTrue(columns.isEmpty());
+    cursor.close();
+    db.close();
+  }
+
+  private SQLiteDatabase givenWritableDatabase() {
+    return DbCommon.givenWritableDatabase(context);
   }
 
   private HashSet<String> givenAuthorColumns() {
