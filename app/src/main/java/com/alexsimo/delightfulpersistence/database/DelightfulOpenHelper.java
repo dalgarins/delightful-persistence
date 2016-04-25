b0820b0811ca8b792edab5949b1d82ccfdb2450d
Alexandru Simonescu <alexandru.simonescu@gmail.com>
2016-04-25 22:56:21 +0200

    Added Migration_v2.sq; for migrating at DB v2

--- app/src/main/java/com/alexsimo/delightfulpersistence/database/DelightfulOpenHelper.java
+++ app/src/main/java/com/alexsimo/delightfulpersistence/database/DelightfulOpenHelper.java
@@ -7,6 +7,7 @@ import com.alexsimo.delightfulpersistence.database.model.AuthorModel;
 import com.alexsimo.delightfulpersistence.database.model.BookAuthorModel;
 import com.alexsimo.delightfulpersistence.database.model.BookModel;
 import com.alexsimo.delightfulpersistence.database.model.Migration_v1Model;
+import com.alexsimo.delightfulpersistence.database.model.Migration_v2Model;
 import com.alexsimo.delightfulpersistence.database.populator.AuthorPopulator;
 import com.alexsimo.delightfulpersistence.database.populator.BookPopulator;
 
@@ -45,6 +46,9 @@ public class DelightfulOpenHelper extends SQLiteOpenHelper {
   public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     if (oldVersion < 2) {
       db.execSQL(Migration_v1Model.MIGRATE_AUTHOR);
+    }
+    if (oldVersion < 3) {
+      db.execSQL(Migration_v2Model.MIGRATE_BOOK);
     }
   }
 }
